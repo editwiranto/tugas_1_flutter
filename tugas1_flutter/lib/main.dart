5516277df97868b816b7772d9a7640beadbf1f74
Edit-wiranto <editwiranto@gmail.com>
2022-04-19 23:59:17 +0700

    Tugas 1 Niomic
    Tugas ini untuk Niomic

---
+++ tugas1_flutter/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Tugas1_flutter()));
+}
+
+class Tugas1_flutter extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("SELAMAT BELAJAR"),
+        ),
+      ),
+    );
+  }
+}
